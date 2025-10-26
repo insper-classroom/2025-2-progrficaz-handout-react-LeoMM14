08aff9348ba7ba22c21288d9d8f9f85080a46272
Léo <leomm@al.insper.edu.br>
2025-10-26 19:35:51 -0300

    aleatorio bonus

--- notes-frontend/src/components/Note/index.jsx
+++ notes-frontend/src/components/Note/index.jsx
@@ -1,9 +1,28 @@
 import "./index.css";
+import { use, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export default function Note(props) {
+    const [rotation, setRotation] = useState(0);
+  useEffect(() => {
+    setRotation(randomInt(-5, 5));
+  }, []);
+
+    const [color, setcolor] = useState("#fae890");
+  useEffect(() => {
+    const colors = ["#ead3a7", "#9de0f5", "#ef89ba", "#fae890", "#abe9c1"];
+    setcolor(colors[randomInt(0, colors.length - 1)]);
+  }, []);
+
+  const style = { transform: `rotate(${rotation}deg)`, backgroundColor: color };
   return (
-    <div className="card">
+    <div className="card" style={style}>
       {/* Botão visual de lixeira (apenas HTML/CSS) */}
       <button
         type="button"
